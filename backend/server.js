495284e3ce60db30720d0e3da17fd591218d7869
Rajiv <bttmndl@gmail.com>
2022-12-16 20:14:05 +0530

    server intial setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const { chats } = require("./data/data")
 
 const app = express();
 app.get('/', (req, res)=>{
     res.send("Api is runing")
 })
+
+app.get("/api/chat", (req, res)=>{
+    res.send(chats);
+})
+
+app.get("/api/chat/:id" , (req, res)=>{
+    // console.log(req.params.id);
+    const singleChat = chats.find((c)=> c._id===req.params.id);
+    res.send(singleChat)
+})
+
 
 app.listen(5000, console.log("Server started at port 5000"));
