28eba15a575bebe44ed32f26b7327eadb029503c
Rajiv <bttmndl@gmail.com>
2022-12-17 18:54:55 +0530

    chat schema completed

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ChatPage() {
+  const [chats, setChats] = useState([]);
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
   return (
-    <div>ChatPage</div>
-  )
+    <div>
+      {chats.map((chat) => {
+        return(<div key= {chat._id}>{chat.chatName}</div>);
+      })}
+    </div>
+  );
 }
